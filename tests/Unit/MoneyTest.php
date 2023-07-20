33119a8413ea492d763098ff972e64dc8aa1d5d4
Nikolai Shangin <shanginn@gmail.com>
2023-07-20 12:55:57 +0800

    Init tests for Money class

---
+++ tests/Unit/MoneyTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\DataObjects\Money;
+use PHPUnit\Framework\TestCase;
+
+class MoneyTest extends TestCase
+{
+    public function testAdd()
+    {}
+
+    public function testSubtract()
+    {}
+
+    public function testMultiply()
+    {}
+
+    public function testDivide()
+    {}
+
+    public function testRound()
+    {}
+
+    public function testFloor()
+    {}
+
+    public function testToString()
+    {}
+}
