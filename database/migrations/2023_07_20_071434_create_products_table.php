c627cb31b66fbfcbddb46a3ce5288af7f833a182
Nikolai Shangin <shanginn@gmail.com>
2023-07-20 19:14:28 +0800

    Create migration for Product

--- database/migrations/2023_07_20_071434_create_products_table.php
+++ database/migrations/2023_07_20_071434_create_products_table.php
@@ -14,6 +14,13 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('title');
+            $table->float('discount')->nullable();
+            $table->unsignedDecimal(
+                column: 'price',
+                total: 8,
+                places: 2
+            );
         });
     }
 
